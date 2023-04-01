9728c8fa0c27374c62f583829e67249d585875cc
YOVANNYOCHOA <yovannyandresochoa@gmail.com>
2023-04-01 12:43:17 -0500

    New Nent Category

---
+++ ECBDatos.git/ECBDatos.git/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using ECBDatos.git.DAL;
+using ECBDatos.git.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ECBDatos.git.Controllers
+{
+    public class CategoriesController:Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+                return  View(await _context.Categories.ToListAsync());
+                        
+        }
+
+
+
+    }
+}
